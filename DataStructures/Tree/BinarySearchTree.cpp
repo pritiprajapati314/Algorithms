46a2ce47cf4f98dbf70b32920785de15c1cfde7c
pritiprajapati314 <pritiprajapati314@gmail.com>
2020-05-12 17:34:38 +0530

    "binary search tree"

--- DataStructures/Tree/BinarySearchTree.cpp
+++ DataStructures/Tree/BinarySearchTree.cpp
@@ -2,251 +2,235 @@
 using namespace std;
 
 class Node{
-public:
+    public:
     int data;
     Node* parent;
-    Node* left;
     Node* right;
-
+    Node* left;
     Node(){
-        this->data=-1;
-        this->parent=NULL;
-        this->left=NULL;
-        this->right=NULL;
+        this->data = -1;
+        this->parent = NULL;
+        this->right = NULL;
+        this->left = NULL;
     }
-    Node(int value){
-        this->data=value;
-        this->parent=NULL;
-        this->left=NULL;
-        this->right=NULL;
+    Node(int data){
+        this->data = data;
+        this->parent = NULL;
+        this->right = NULL;
+        this->left = NULL;
+    }
+    Node(int data, Node* parent){
+        this->data = data;
+        this->parent = parent;
+        this->right = NULL;
+        this->left = NULL;
+    }
+
+    void setBothChild(Node* leftChild, Node* rightChild){
+        this->left = leftChild;
+        this->right = rightChild;
+        leftChild->parent = this;
+        rightChild->parent = this;
+    }
+
+    void setOnlyRightChild(Node* rightChild){
+        this->right = rightChild;
+        rightChild->parent = this;
+    }
+
+    void setOnlyLeftChild(Node* leftChild){
+        this->left = leftChild;
+        leftChild->parent = this;
     }
 };
 
 
-Node* insert(Node* current_node, int value){
-    if(current_node == NULL){
-        return new Node(value);
-    }
-    else if(current_node->data > value){
-        current_node->left = insert(current_node->left, value);
-    }
-    else{
-        current_node->right = insert(current_node->right, value);
-    }
-    return current_node;
-}
-
-void inorderTraversal(Node* current_node){
-    if(current_node != NULL){
-        inorderTraversal(current_node->left);
-        cout<< current_node->data << " ";
-        inorderTraversal(current_node->right);
-    }
-}
-
-void preorderTraversal(Node* current_node){
-    if(current_node != NULL){
-        cout<< current_node->data << " ";
-        preorderTraversal(current_node->left);
-        preorderTraversal(current_node->right);
-    }
-}
-
-
-void postorderTraversal(Node* current_node){
-    if(current_node != NULL){
-        postorderTraversal(current_node->left);
-        postorderTraversal(current_node->right);
-        cout<< current_node->data << " ";
-    }
-}
-
-
-Node* maximum(Node* current_node){
-    Node* ptr = current_node;
-
-    while(ptr->right != NULL){
-        ptr = ptr->right;
-    }
-
-    return ptr;
-}
-
-Node* minimum(Node* current_node){
-    Node* ptr = current_node;
-
-    while(ptr->left != NULL){
-        ptr = ptr->left;
-    }
-
-    return ptr;
-}
-
-bool find(Node* current_node, int value){
-
-    if(value == current_node->data){
-        return true;
-    }
-    else if(current_node->data > value){
-        return find(current_node->left, value);
-    }
-    else {
-        return find(current_node->right, value);
-    }
-
-    return false;
-}
-
-Node* findNode(Node* currentNode, int value){
+// inorder traversal for binary search tree
+void inorderTraversal(Node* currentNode){
     if(currentNode != NULL){
-        if(currentNode->data == value){
-            return currentNode;
-        }
-        else if(currentNode->data > value){
-            return findNode(currentNode->left, value);
-        }
-        else{
-            return findNode(currentNode->right, value);
-        }
+        inorderTraversal(currentNode->left);
+        cout<< currentNode->data<< " ";
+        inorderTraversal(currentNode->right); 
     }
-    else{
+}
+
+// returns the node in binary search tree whose data is equal to value
+// returns NULL if there is no such node
+Node* find(Node* currentNode, int value){
+    if(currentNode == NULL){
         return NULL;
     }
-}
-
-
-void successor(Node* current_node, int& s, int key){
-
-    if(current_node == NULL){
-        return;
+    if(currentNode->data == value){
+        return currentNode;
     }
-    if(current_node->data == key){
-    if(current_node->right != NULL){
-        Node* temp = current_node->right;
-        s = temp->data;
-        while(temp->left != NULL){
-            s = temp->data;
-            temp = temp->left;
-        }
-    }
-    }
-    if(current_node->data > key){
-        s = current_node->data;
-        successor(current_node->left, s, key);
+    if(currentNode->data > value){
+        return find(currentNode->left, value);
+        //cout<< currentNode->data << " " <<  currentNode<< endl;
     }
     else{
-        successor(current_node->right, s, key);
+        return find(currentNode->right, value);
+        //cout<< currentNode->data << " " << currentNode<< endl;
     }
-    return;
 }
 
-Node* predecessor(Node* current_node, int& p, int key){
-    if(current_node == NULL){
-        return NULL;
-    }
-    if(current_node->data == key){
-    if(current_node->left != NULL){
-        Node* temp = current_node->left;
-        p = temp->data;
-        while(temp->right != NULL){
-            temp = temp->right;
-            p = temp->data;
-        }
-    }
-    }
-    if(current_node->data > key){
-        //cout<<current_node->data<<endl;
-        predecessor(current_node->left, p, key);
+// Returns the minimum value in a binary search tree i.e leftmost node 
+Node* minVal(Node* currentNode){
+    if(currentNode->left == NULL){
+        return currentNode;
     }
     else{
-        if(current_node->data < key)
-            p = current_node->data;
-        //cout<<p<<" "<<key<<endl;
-        predecessor(current_node->right, p, key);
-    }
-    return current_node;
-}
-
-
-// determine if the currentNode is left child or right child
-Node* determineChild(Node* currentNode) {
-    if(currentNode->parent->left == currentNode){
-        return currentNode->parent->left;
-    }else{
-        return currentNode->parent->right;
+        return minVal(currentNode->left);
     }
 }
 
-// determine if the currentNode is a left child or right child
-void deleteNode(Node* currentNode){
-    // case 1: currentNode is a leaf ( No childrens)
+// returns the minimum value in a binary serch tree i.e rightmost node
+Node* maxVal(Node* currentNode){
+    if(currentNode->right == NULL){
+        return currentNode;
+    }
+    else{
+        return maxVal(currentNode->right);
+    }
+}
+
+// returns the successor node of the currentNode
+// returns NULL if there is no successor to the currentNode
+Node* successor(Node* currentNode){
+    if(currentNode->right != NULL){
+        return(minVal(currentNode->right));
+    }
+    Node* y = currentNode->parent;
+
+    while(y != NULL && currentNode == y->right){
+        currentNode = y;
+        y = y->parent;
+    }
+    return(y);
+}
+
+// returns the predecessor node of the currentNode
+// returns NULl if there is no predecessor to the currentNode
+Node* predecessor(Node* currentNode){
+    if(currentNode->left != NULL){
+        return(maxVal(currentNode->left));
+    }
+    Node* y = currentNode->parent;
+
+    while(y != NULL && currentNode == y->right){
+        currentNode = y;
+        y = y->parent;
+    }
+    return(y);
+}
+
+
+//Insert a new node in binary search tree
+//genral function call syntax:
+//insert(rootNode, parent, valueToInsert);
+Node* insert(Node* currentNode, Node* currentNodeParent, int data){
+    if(currentNode == NULL){
+        return new Node(data, currentNodeParent);
+    }
+    else if(currentNode->data> data){
+        currentNode->left= insert(currentNode->left, currentNode, data);
+    }
+    else{
+        currentNode->right = insert(currentNode->right, currentNode, data);
+    }
+    
+    return currentNode;
+}
+
+void deleteNode(Node* currentNode, int data){
+    currentNode = find(currentNode,data);
+
+    //for leaf
     if(currentNode->left == NULL && currentNode->right == NULL){
-        Node* n = determineChild(currentNode);
-        n = NULL;        
-    }
-    // case 2: currentNode has only one child (either left or right)
-    else if(currentNode->left != NULL || currentNode->right != NULL){
-        if(currentNode->left != NULL){                  // if currentNode has only left child
-            Node* n = determineChild(currentNode);
-            n = currentNode->left;
-        }else{                                          // if currentNode has only right child
-            Node* n = determineChild(currentNode);
-            n = currentNode->right;
+        if(currentNode == currentNode->parent->left){
+            currentNode->parent->left = NULL;
+        }else{
+            currentNode->parent->right = NULL;
         }
     }
-    // case 3: currentNode has two children
-    else{
-        int p;
-        Node* predecessorNode = predecessor(currentNode,p,currentNode->data);
-        currentNode->data = predecessorNode->data;
-        deleteNode(predecessorNode);
+
+    //for one child
+    //one left child
+    if(currentNode->left != NULL && currentNode->right == NULL){
+        currentNode->left->parent = currentNode->parent;
+        if(currentNode == currentNode->parent->left){
+            currentNode->parent->left = currentNode->left;
+        }else{
+            currentNode->parent->right = currentNode->left;
+        }
     }
-    cout<<"task completed"<<endl;
+
+    //for one child
+    //one right child
+    if(currentNode->right != NULL && currentNode->left == NULL){
+        currentNode->right->parent = currentNode->parent;
+        if(currentNode == currentNode->parent->right){
+            currentNode->parent->right = currentNode->right;
+        }else{
+            currentNode->parent->left = currentNode->right;
+        }
+    }
+
+    //for two child
+    if(currentNode->right != NULL && currentNode->left != NULL){
+        Node* pv = predecessor(currentNode);
+        Node* sv = successor(currentNode);
+        if( pv->data != -1){
+            currentNode->data = pv->data;
+            delete(pv);
+        }else{
+            currentNode->data = sv->data;
+            delete(sv);
+        }
+    }
 }
 
+//function for testing binary search tree functions
+void testBinarySearchTree(){
+    Node* five = new Node(5);
+    Node* three = new Node(3);
+    Node* one = new Node(1);
+    Node* four = new Node(4);
+    Node* two = new Node(2);
+    Node* seven = new Node(7);
+    Node* nine = new Node(9);
+    Node* eight = new Node(8);
+
+    five->setBothChild(three, seven);
+    three->setBothChild(one, four);
+    one->setOnlyRightChild(two);
+    seven->setOnlyRightChild(nine);
+    nine->setOnlyLeftChild(eight);
 
 
+    // check insert function
+    cout<< "checking insert function ...... " <<endl;
+
+    deleteNode(five,8);
+    inorderTraversal(five);
+    cout<<endl;
+    insert(five, NULL, 15);
+    inorderTraversal(five);
+    cout<<endl<< endl;
+
+    // checking find function
+    cout<< "checking find function ...... "<<endl;
+    Node* node = find(five, 9);
+    cout<< node->data<< endl;
+    cout<< endl;
+
+    // checking predecessor function
+    cout<< "checking predecessor function ...... "<< endl;
+    Node* predecessorNode = predecessor(nine);
+    cout<< predecessorNode->data;
+    cout<< endl;
+}
 
 int main(){
-    Node n, *head=NULL;
-    Node ten=10, six=6, eleven=11, twelve=12, three=3, eight=8, fourteen=14; 
-    ten.left = &six;
-    ten.right = &twelve;
-
-    six.left = &three;
-    six.right = &eight;
-
-    twelve.left = &eleven;
-    twelve.right = &fourteen;
-    insert(&ten, 5);
-    insert(&ten, 1);
-    insert(&ten, 4);
-    insert(&ten, 7);
-    insert(&ten, 9);
-    insert(&ten, 13);
-    
-    cout<< "Inorder Traversal: "; inorderTraversal(&ten);
-    cout<< endl;
-    cout<< "Preorder Traversal: "; preorderTraversal(&ten);
-    cout<< endl;
-    cout<< "Postorder Traversal: "; postorderTraversal(&ten);
-    cout<< endl;
-    Node* max = maximum(&ten);
-    cout<< "Maximum Element: " << max->data;
-    cout<< endl;
-    Node* min = minimum(&ten);
-    cout<< "Minimum Element: " <<min->data;
-    cout<<endl;
-    cout<<"if the foud returns 1 else return 0 : "<<find(&ten,6);
-    cout<<endl;
-    int s = -1, p = -1;
-    successor(&ten, s, 14);
-    predecessor(&ten, p, 14);
-    cout<<"successor : "<<s<<endl;
-    cout<<"predecessor : "<<p<<endl;
-    deleteNode(&six);
-    cout<< "Inorder Traversal: "; inorderTraversal(&ten);
-    cout<< endl;
+    testBinarySearchTree();
     return 0;
 }
-
