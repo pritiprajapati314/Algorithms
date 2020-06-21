103ad69b05e2f9b2d079924a275948943feb3569
pritiprajapati314 <pritiprajapati314@gmail.com>
2020-06-21 13:02:10 +0530

    added: solved longest common subsequence

--- Algorithms/DynamicProgramming/longestCommonSubsequence.cpp
+++ Algorithms/DynamicProgramming/longestCommonSubsequence.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 #include <vector> 
 #include <string>
-#define DEFAULR L
 
 using namespace std;
+
+int maximum(int a, int b){
+    if(a > b){
+        return a;
+    }else{
+        return b;
+    }
+}
 
 int longestCommonSequence(string first, string second, int lenF, int lenS){
     if(lenF == 0 || lenS == 0 ){
         return 0; 
     }
+
     if(first[lenF-1] == second[lenS -1]){
         cout<<first[lenF-1]<<" ";
         return (1 + longestCommonSequence(first, second, lenF-1, lenS-1));
     }
     else{
-        return max(longestCommonSequence(first, second, lenF-1, lenS),
+        return maximum(longestCommonSequence(first, second, lenF-1, lenS),
                    longestCommonSequence(first, second, lenF, lenS-1));
     }
 }
@@ -22,11 +30,11 @@ int longestCommonSequence(string first, string second, int lenF, int lenS){
 void testCaseLCS(){
     string first = "babbab";
     string second = "abaaba";
-    string sequence;
-
+    vector<string>sequence;
    
     cout<<longestCommonSequence(first, second, first.size(), second.size());
 }
+
 
 int main(){
     testCaseLCS();
