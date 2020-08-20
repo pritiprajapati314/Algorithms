a42ef856aef01cb8e0c22195989b4fe875468712
pritiprajapati314 <pritiprajapati314@gmail.com>
2020-08-20 20:51:09 +0530

    dg

--- DataStructures/SortingAndSearching/BubbleSort.cpp
+++ DataStructures/SortingAndSearching/BubbleSort.cpp
@@ -5,11 +5,11 @@ using namespace std;
 void bubbleSort(int arr[], int length){
     int temp;
     for(int i = 0; i < length; i++){
-        for(int j = 0; j < length; j++){
-            if(arr[i] < arr[j]){
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+        for(int j = 0; j < length-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
     }
